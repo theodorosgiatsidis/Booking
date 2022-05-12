fc37c4baa0821140e5c531772b13892293806d67
Thodoros Giatsidis <46629765+theodorosgiatsidis@users.noreply.github.com>
2022-05-12 11:31:23 +0300

    express api

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend");
+});
