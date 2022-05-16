a9daff4368fd8274bfc552eafcf5b07e03bc79c7
Thodoros Giatsidis <46629765+theodorosgiatsidis@users.noreply.github.com>
2022-05-16 17:38:10 +0300

    featuredProperties ajax call

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">GevrisBooking</span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">GevrisBooking</span>
+        </Link>
         <div className="navItems">
           <button className="navButtons">Login</button>
           <button className="navButtons">Register</button>
