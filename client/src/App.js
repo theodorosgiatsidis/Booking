f229f0fc2be24dc6c1abc3bd932e4f63e7d54f30
Thodoros Giatsidis <46629765+theodorosgiatsidis@users.noreply.github.com>
2022-05-18 15:28:07 +0300

    booking API

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 import "./App.css";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
         <Route path="/hotels/:id" element={<Hotel />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,38 @@
 import "./navbar.css";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { user, dispatch } = useContext(AuthContext);
+
+  const handleClick = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">GevrisBooking</span>
         </Link>
-        <div className="navItems">
-          <button className="navButtons">Login</button>
-          <button className="navButtons">Register</button>
-        </div>
+        {user ? (
+          <div>
+            <span>{user.username}</span>
+            <span onClick={handleClick} className="logout">
+              Logout
+            </span>
+          </div>
+        ) : (
+          <div className="navItems">
+            <Link to="/login">
+              <button className="navButtons">Login</button>
+            </Link>
+            <Link to="/register">
+              <button className="navButtons">Register</button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
