9ccbd178c292174b8df7ec387aa58337f62e5fd3
Thodoros Giatsidis <46629765+theodorosgiatsidis@users.noreply.github.com>
2022-05-24 11:06:49 +0300

    heroku test

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -55,7 +55,7 @@ const Reserve = ({ setOpen, hotelId }) => {
     try {
       await Promise.all(
         selectedRooms.map((roomId) => {
-          const res = axios.put(`/rooms/availability/${roomId}`, {
+          const res = axios.put(`/api/rooms/availability/${roomId}`, {
             dates: alldates,
           });
           return res.data;
